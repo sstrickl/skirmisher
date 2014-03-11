a7e2a489e2b305e2ff0b60ea2989930e705185f9
aking <psy_wombats@wombatrpgs.net>
2014-03-11 15:39:49 -0400

    Basic class addon structure/template

---
+++ init.lua
@@ -0,0 +1,29 @@
+-- Skirmisher, a class for Tales of Maj'Eyal 1.1.5
+-- contact: psy_wombats@wombatrpgs.net
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+long_name = "Skirmisher class"
+short_name = "skirmisher"
+for_module = "tome"
+version = {1,1,5}
+weight = 1
+author = { "Skirmisher team", "psy_wombats@wombatrpgs.net" }
+homepage = "..."
+description = [[Skirmisher: a sling and shield-wielding warrior]]
+tags = {'skirmisher', 'archer', 'warrior', 'slingwark', 'class'}
+overload = false
+superload = false
+hooks = true
+data = true
